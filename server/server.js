684f4c3a2adca0bb14b39ee890d3caa5ad1e1370
Krystal <krylopez@cisco.com>
2023-04-04 11:38:35 -0700

    created test text file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const app = require('./app');
+const app = require("./app");
 
-const {PORT = 3000} = process.env;
+const { PORT = 4000 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
